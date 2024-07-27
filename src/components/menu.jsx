6ec3202fc8b9e9604606d61801910ea5e96a03cd
williamleo10 <williamwalangitan23@gmail.com>
2024-07-27 11:22:01 +0700

    menu bar

---
+++ src/components/menu.jsx
@@ -0,0 +1,10 @@
+
+export default function MenuBar() {
+    return (
+        <>
+            <aside className="menu-bar">
+                <img className="logo-container" src="./menu-logo.svg" alt="" />
+            </aside>
+        </>
+    )
+}
